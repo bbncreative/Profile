8aabb18ec1be7d57aa7f5a51f538ba708b87f7be
Aaron Cawte <aaron@bbncreative.co>
2019-03-03 12:28:40 +0000

    CircleCI Configured, API Keys moved

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import {FIREBASE_API_KEY, FIREBASE_MESSAGING_ID} from "./secrets";
 
 const config = {
-  apiKey: FIREBASE_API_KEY,
+  apiKey: "AIzaSyCYVSx7ov3c2S_1MdUK54HCUdNN-UQzmOA",
   authDomain: "profile-by-bbncreative.firebaseapp.com",
   databaseURL: "https://profile-by-bbncreative.firebaseio.com",
   projectId: "profile-by-bbncreative",
   storageBucket: "",
-  messagingSenderId: FIREBASE_MESSAGING_ID
+  messagingSenderId: "677029636638"
 };
 firebase.initializeApp(config);
 export default firebase;
